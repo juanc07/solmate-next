71181da37ce62e9011e0cfcc14d065af024806b7
Juan Carlos Claridad <juanc.claridad@gmail.com>
2024-11-17 23:05:55 +0800

    hide the scrollbar in WEPE claim page

--- src/components/custom/client/ClaimWepe.tsx
+++ src/components/custom/client/ClaimWepe.tsx
@@ -14,9 +14,9 @@ const ClaimWepe = () => {
   }, []);
 
   return (
-    <div className="min-h-screen flex transition-colors duration-300 bg-white text-black dark:bg-black dark:text-white">
-      <main className="flex-1 flex flex-col h-screen overflow-hidden">
-        <div className="flex-1 overflow-auto p-6 sm:p-8 md:p-10 lg:p-12">
+    <div className="min-h-screen w-full flex transition-colors duration-300 bg-white text-black dark:bg-black dark:text-white overflow-hidden">
+      <main className="flex-1 flex flex-col min-h-screen overflow-hidden">
+        <div className="flex-1 overflow-y-auto overflow-x-hidden p-6 sm:p-8 md:p-10 lg:p-12">
           <ClaimWepeContent />
         </div>
       </main>

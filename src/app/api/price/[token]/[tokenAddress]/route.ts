7a2ca71f7d72acdecf3fde6f4c047a032569d21f
Juan Carlos Claridad <juanc.claridad@gmail.com>
2024-12-15 23:13:52 +0800

    fixing getting token price using jupiter api and coingekco

--- src/app/api/price/[token]/[tokenAddress]/route.ts
+++ src/app/api/price/[token]/[tokenAddress]/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const JUPITER_API_URL = "https://price.jup.ag/v6/price";
+//const JUPITER_API_URL = "https://price.jup.ag/v6/price";
+const JUPITER_API_URL = "https://api.jup.ag/price/v2";
 const COINGECKO_API_URL = "https://api.coingecko.com/api/v3/simple/price";
 const WSOL_ID = "So11111111111111111111111111111111111111112"; // Wrapped SOL ID
 
@@ -9,13 +10,13 @@ async function getPriceFromJupiter(tokenAddress: string): Promise<number | null>
   try {
     console.log(`Fetching price for ${tokenAddress} from Jupiter API`);
     const response = await fetch(`${JUPITER_API_URL}?ids=${tokenAddress}`);
-    
+
     // Check if response is okay before parsing
     if (!response.ok) {
       console.error(`Error fetching from Jupiter API: ${response.status} ${response.statusText}`);
       return null;
     }
-    
+
     // Parse JSON and handle response structure
     const data = await response.json();
     console.log(`Fetching price for ${tokenAddress} from Jupiter API got price: ${data?.data?.[tokenAddress]?.price}`);
@@ -32,7 +33,14 @@ async function getPriceFromCoinGecko(token: string): Promise<number | null> {
   try {
     console.warn(`Falling back to CoinGecko API for ${token}`);
     const response = await fetch(
-      `${COINGECKO_API_URL}?ids=${token.toLowerCase()}&vs_currencies=usd`
+      `${COINGECKO_API_URL}?ids=${token.toLowerCase()}&vs_currencies=usd`,
+      {
+        method: "GET", // Specify the request method (optional, defaults to GET)
+        headers: {
+          Accept: "application/json", // Set the Accept header
+          "x-cg-pro-api-key": "CG-2Q1hdsQ7FBFEVaxx3qU3Hrwd ", // Replace with your actual API key
+        },
+      }
     );
 
     // Check if response is JSON
@@ -64,14 +72,14 @@ export async function GET(
 
   if (price === null && token === "SOL") {
     //console.warn(`Jupiter API failed for ${token}. Trying WSOL ID...`);
-    price = await getPriceFromJupiter(WSOL_ID);    
-  }  
+    price = await getPriceFromJupiter(WSOL_ID);
+  }
 
   // If Jupiter fails, try to fetch the price from CoinGecko
   if (price === null) {
     console.warn(`Jupiter API failed for ${token}. Falling back to CoinGecko.`);
     price = await getPriceFromCoinGecko(token);
-  } 
+  }
 
   // If both Jupiter and CoinGecko fail, return an error
   if (price === null) {
